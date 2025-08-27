4fb03af4c9856101ac301568e3552f1d293df7e4
Arcemist <arcemisto@gmail.com>
2025-08-27 18:57:56 -0300

    añadido la tarea de listas

--- Clase_1/E10.py
+++ Clase_1/E10.py
@@ -3,22 +3,23 @@
 # y segun un input de "M" o "V" o "T" muestre un mensaje de
 # "Buen Dia!" o "Buenas Tardes!" o "Buenas Noches!"
 # o "¡Valor no válido!", segun el caso.
+# Fe de errata: cambiado para que se pueda usar la letra en minuscula
+# Debido a la opiñon del profe en la siguiente clase
 
 print("""
 Introduce tu horario segun esta regla:
-    M para Mañana
-    V para Tarde
-    N para Noche
+    M - Mañana
+    V - Tarde
+    N - Noche
 """)
 
 horario = input()
 
-
-if horario == "M":
+if horario == "M" or "m":
     print("Buen Dia!")
-elif horario == "V":
+elif horario == "V" or "v":
     print("Buenas Tardes!")
-elif horario == "N":
+elif horario == "N" or "n":
     print("Buenas Noches!")
 else:
     print("¡Valor no válido!")
---
+++ ClasesDeRepaso.md
@@ -0,0 +1,2 @@
+Lista de Clases que fueron solo de repaso:
+  Clase 2: Fue un repaso de la tarea de casa de la clase 1
